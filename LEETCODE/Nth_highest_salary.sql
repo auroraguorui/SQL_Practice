e9f58c1c39e415966dbe658bead48768841e9b5b
auroraguorui <auroraguorui@hotmail.com>
2016-01-19 17:35:32 -0800

    Update Nth_highest_salary.sql

--- LEETCODE/Nth_highest_salary.sql
+++ LEETCODE/Nth_highest_salary.sql
@@ -16,7 +16,7 @@ CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
   RETURN (
      # Write your MySQL query statement below.
-     SELECT MAX(Salary) /*This is the outer query part */
+     SELECT DISTINCT Salary /*This is the outer query part */
             FROM Employee Emp1
             WHERE (N-1) = ( /* Subquery starts here */
                  SELECT COUNT(DISTINCT(Emp2.Salary))
