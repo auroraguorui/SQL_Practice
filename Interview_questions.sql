bdd21801d0f344a0e09e4723517ab0aa2b8fd71c
auroraguorui <auroraguorui@hotmail.com>
2016-01-19 17:34:18 -0800

    Update Interview_questions.sql

--- Interview_questions.sql
+++ Interview_questions.sql
@@ -33,4 +33,47 @@ HAVING COUNT(*) > 1
 -- # 1d. Write a SQL statement to insert rows into a table called highAchiever(Name, Age), where a salesperson must have a salary of 
 --       100,000 or greater to be included in the table.
 INSERT INTO highAchiever(Name, Age)
-VALUES ()
+VALUES ( 
+  SELECT Name, Age 
+  FROM   Salesperson
+  WHERE  Salary >= 100000
+)
+
+-- # 2. This question was asked in a Google interview: Given the 2 tables below, User and UserHistory:
+# +-----------+  +-------------+
+# | User      |  | UserHistory |
+# +-----------+  +-------------+
+# | user_id   |  | user_id     |
+# | name      |  | date        |
+# | phone_num |  | action      |
+# +-----------+  +-------------+
+-- # 2a. Write a SQL query that returns the name, phone number and most recent date for any user that has logged in over the last 30 days 
+--       (you can tell a user has logged in if the action field in UserHistory is set to "logged_on"). Every time a user logs in a new 
+--       row is inserted into the UserHistory table with user_id, current date and action (where action = "logged_on").
+SELECT name, phone_num, MAX(date)
+FROM   User 
+JOIN   UserHistory ON UserHistory.user = User.user_id
+WHERE  UserHistory.action = 'logged_on' AND UserHistory.date > date_sub(curdate(), interval 30 day) 
+GROUP BY User.user_id
+-- # 2b. Write a SQL query to determine which user_ids in the User table are not contained in the UserHistory 
+--       table (assume the UserHistory table has a subset of the user_ids in User table). Do not use the SQL MINUS statement. Note: the
+--       UserHistory table can have multiple entries for each user_id.
+SELECT DISTINCT User.user_id 
+FROM   User
+LEFT JOIN UserHistory ON User.user_id = UserHistory.user_id
+WHERE  UserHistory.user_id = NULL
+
+-- # 3. Let’s say that you are given a SQL table called “Compare” (the schema is shown below) with only one column called “Numbers”.
+--      Write a SQL query that will return the maximum value from the “Numbers” column, without using a SQL aggregate like MAX or MIN.
+SELECT Numbers 
+FROM   Compare
+ORDER BY Numbers DESC
+LIMIT  1
+
+SELECT DISTINCT Numbers
+FROM   Compare
+WHERE  Numbers NOT IN (
+  SELECT Smaller.Numbers
+  FROM   Compare AS Larger
+  JOIN   Compare AS Smaller ON Smaller.Numbers < Larger.Numbers )
+  
