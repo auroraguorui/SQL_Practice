4598a84580db744ea4878e9a379a5652835bbca5
auroraguorui <auroraguorui@hotmail.com>
2016-05-05 17:07:42 -0700

    Update Rising_Temperature.sql

--- LEETCODE/Rising_Temperature.sql
+++ LEETCODE/Rising_Temperature.sql
@@ -39,3 +39,9 @@ SELECT wt1.Id
 FROM Weather wt1, Weather wt2
 WHERE wt1.Temperature > wt2.Temperature AND 
       TO_DAYS(wt1.DATE) - TO_DAYS(wt2.DATE) = 1;
+
+
+# Write your MySQL query statement below
+SELECT w1.Id
+FROM Weather w1
+JOIN Weather w2 ON DATEDIFF(w1.Date, w2.Date)=1 AND w1.temperature > w2.temperature
